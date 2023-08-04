52317d13e732b919762ed2e0aca319b172e3aeec
Leonardo8221 <r.leonardo0802@gmail.com>
2023-08-04 16:03:13 -0700

    remove other aws stuff

--- src/utils/aws-config.js
+++ src/utils/aws-config.js
@@ -1,15 +1,9 @@
-import * as AWS from "aws-sdk/global";
 import {
     CognitoUserPool,
   } from "amazon-cognito-identity-js";
 
-AWS.config.region = "us-east-1";
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-  IdentityPoolId: "us-east-1:4f690679-91cd-4953-9d7e-75fd62d77d4c",
-});
-
 var poolData = {
-  userPoolId: "us-east-1_n9kuj2vYy",
-  userPoolWebClientId: "1k5m7ubtbja69k2skp40um693i",
+  UserPoolId: "us-east-1_n9kuj2vYy",
+  ClientId: "1k5m7ubtbja69k2skp40um693i",
 };
 export const userPool = new CognitoUserPool(poolData);
