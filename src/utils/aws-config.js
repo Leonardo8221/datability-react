57d344a09ff3ea545d6a858033b78e9a20cc2746
Leonardo8221 <r.leonardo0802@gmail.com>
2023-08-03 22:06:01 -0700

    added values directly to js file

--- src/utils/aws-config.js
+++ src/utils/aws-config.js
@@ -3,13 +3,13 @@ import {
     CognitoUserPool,
   } from "amazon-cognito-identity-js";
 
-AWS.config.region = process.env.REACT_APP_AWS_REGION;
+AWS.config.region = "us-east-1";
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-  IdentityPoolId: process.env.REACT_APP_AWS_IDENTITY_POOL_ID, // your identity pool id here
+  IdentityPoolId: "us-east-1:4f690679-91cd-4953-9d7e-75fd62d77d4c",
 });
 
 var poolData = {
-  UserPoolId: 'us-east-1_n9kuj2vYy',
-  ClientId: '1k5m7ubtbja69k2skp40um693i',
+  UserPoolId: "us-east-1_n9kuj2vYy",
+  ClientId: "1k5m7ubtbja69k2skp40um693i",
 };
 export const userPool = new CognitoUserPool(poolData);
